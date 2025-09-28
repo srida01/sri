01f1c29abe47182d171b1b4dc01687b317f0743a
Narra shanmukha srida <nsrida.241cs238@nitk.edu.in>
2025-09-28 07:14:19 +0530

    Add .

--- OneDrive/Desktop/learnhub/main.py
+++ OneDrive/Desktop/learnhub/main.py
@@ -30,7 +30,7 @@ allow_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
